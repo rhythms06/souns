aeafa0b4d0b306220b1d7c16bdecb52c85eb1327
Sakib Rasul <rhythms06@gmail.com>
2023-03-28 22:44:44 -0300

    fix: Specify types in layout.tsx

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -1,9 +1,10 @@
 import { Inter } from "next/font/google";
 import Header from "./header";
+import React from "react";
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Layout({ children }) {
+export default function Layout({ children } : { children: React.ReactNode }) {
     return (
         <div className={inter.className}>
             <Header />
