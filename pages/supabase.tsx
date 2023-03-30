e4612f2793fb9d9a431bea9baa38e969f5d23ba9
Sakib Rasul <rhythms06@gmail.com>
2023-03-29 22:32:16 -0300

    feat: install @tailwindcss/forms, react-drag-drop-files, and @supabase/supabase-js

---
+++ pages/supabase.tsx
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+    'https://hrcyxwfhttrflcuvtqzy.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyY3l4d2ZodHRyZmxjdXZ0cXp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAxMjYwODcsImV4cCI6MTk5NTcwMjA4N30.bnJgeqvbZ5t6DPaXESw7I8KQMDNiDMVaAZpkPFO4dbE'
+);
